e050f0a3415ad1a23f242893e6fe1828a615b1b5
Hafiz Usman Ali <77631826+Usman-pk@users.noreply.github.com>
2022-06-07 15:06:00 +0500

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,12 @@
+var value = 0;
+function add()
+{
+    value++;
+    document.getElementById("count").innerHTML = value;
+}
+
+function subtract()
+{
+    value--;
+    document.getElementById("count").innerHTML = value;   
+}
